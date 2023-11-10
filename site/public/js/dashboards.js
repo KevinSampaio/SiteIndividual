ba0b2916109ca5e6bb057bb10ab3d7ea54a09276
KevinSampaio <kevin.sampaio@sptech.school>
2023-11-10 18:01:11 -0300

    Inicio da ligação com o WebDataViz

---
+++ site/public/js/dashboards.js
@@ -0,0 +1,25 @@
+const toggleSidebar = () => document.body.classList.toggle("open");
+
+function home(){
+  window.location.href = "../index.html"
+}
+
+function inserirAluno(){
+  window.location.href = "inserirAtleta.html"
+}
+
+function deletarAluno(){
+  window.location.href = "deletarAtleta.html"
+}
+
+function selecionarAluno(){
+  window.location.href = "selecionarAtleta.html"
+}
+
+function consultarIMC(){
+  window.location.href = "consultarIMC.html"
+}
+
+function sair(){
+  window.location.href = "../login.html"
+}
