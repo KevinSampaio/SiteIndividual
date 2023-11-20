74f550ecaed45e6caa5c315cae30aeb22a00544f
KevinSampaio <kevin.sampaio@sptech.school>
2023-11-20 13:51:29 -0300

    Imagens no index

--- site/src/models/alunoModel.js
+++ site/src/models/alunoModel.js
@@ -2,9 +2,9 @@ var database = require("../database/config");
 
 function cadastrar(nome, dtNasc, faixa, peso, altura, professorId, equipeId) {
   
-  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, dtNasc, faixa, peso, altura, professorId, equipeId);
+  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, dtNasc, faixa, peso, altura, equipeId, professorId);
 
-    var query = `insert into aluno (nome, dtNasc, faixa, peso, altura, fk_usuario, fk_equipe) values ('${nome}', '${dtNasc}','${faixa}', '${peso}', '${altura}','${professorId}','${equipeId}')`;
+    var query = `insert into aluno (nome, dtNasc, faixa, peso, altura, fk_usuario, fk_equipe) values ('${nome}', '${dtNasc}','${faixa}', '${peso}', '${altura}','${equipeId}','${professorId}')`;
   
     return database.executar(query);
   }
